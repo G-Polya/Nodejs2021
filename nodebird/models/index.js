6ab9004618a1ea7e2b1277668aea55a3fee67ef0
DESKTOP-60TM4II\KimHeeSu <wlskaka4@gmail.com>
2021-01-29 14:01:29 +0900

    nodebird not completed localLogin is not running

--- nodebird/models/index.js
+++ nodebird/models/index.js
@@ -1,12 +1,14 @@
-const Sequelize = require("sequelize");
+const Sequelize = require('sequelize');
 const env = process.env.NODE_ENV || 'development';
-const config = require("../config/config")[env];
-const User = require("./user");
-const Post = require("./post");
-const Hashtag = require("./hashtag");
+const config = require('../config/config')[env];
+const User = require('./user');
+const Post = require('./post');
+const Hashtag = require('./hashtag');
 
 const db = {};
-const sequelize = new Sequelize(config.database, config.username, config.password, config);
+const sequelize = new Sequelize(
+  config.database, config.username, config.password, config,
+);
 
 db.sequelize = sequelize;
 db.User = User;
@@ -18,7 +20,7 @@ Post.init(sequelize);
 Hashtag.init(sequelize);
 
 User.associate(db);
-Post.assoicate(db);
+Post.associate(db);
 Hashtag.associate(db);
 
 module.exports = db;
