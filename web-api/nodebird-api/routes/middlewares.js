a0e1cad096d1b7b382f89129d9dd0c95bd7c87be
DESKTOP-60TM4II\KimHeeSu <wlskaka4@gmail.com>
2021-02-01 13:57:33 +0900

    routes v2 apiLimiter added

--- web-api/nodebird-api/routes/middlewares.js
+++ web-api/nodebird-api/routes/middlewares.js
@@ -1,4 +1,24 @@
 const jwt = require('jsonwebtoken');
+const RateLimit = require("express-rate-limit")
+
+exports.apiLimiter = new RateLimit({
+  windowMs: 60 * 1000,
+  max:1,
+  delayMs:0,
+  handler(req,res){
+    res.status(this.statusCode).json({
+      code:this.statusCode,
+      meesage:"1분에 한 번만 요청할 수 있습니다"
+    })
+  }
+})
+
+exports.deprecated = (req,res)=>{
+  res.status(410).json({
+    code:410,
+    message:"새로운 버전이 나왔습니다. 새로운 버전을 사용하세요"
+  })
+}
 
 exports.isLoggedIn = (req, res, next) => {
   if (req.isAuthenticated()) {
