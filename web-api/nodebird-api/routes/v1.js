c93bd51e9546f9d2d40524bfa5d20ecd6847efc6
DESKTOP-60TM4II\KimHeeSu <wlskaka4@gmail.com>
2021-02-02 11:25:12 +0900

    .env file removed

--- web-api/nodebird-api/routes/v1.js
+++ web-api/nodebird-api/routes/v1.js
@@ -1,92 +1,99 @@
-const express = require('express');
-const jwt = require('jsonwebtoken');
+const express = require("express");
+const jwt = require("jsonwebtoken");
 
-const { verifyToken } = require('./middlewares');
-const { Domain, User, Post,Hashtag } = require('../models');
+const { verifyToken, deprecated } = require("./middlewares");
+const { Domain, User, Post, Hashtag } = require("../models");
 
 const router = express.Router();
 
-router.get("/posts/my",verifyToken, (req,res)=>{
-  Post.findAll({where:{userId:req.decoded.id}})
-    .then((posts)=>{
-      console.log(posts)
+router.use(deprecated);
+
+router.get("/posts/my", verifyToken, (req, res) => {
+  Post.findAll({ where: { userId: req.decoded.id } })
+    .then((posts) => {
+      console.log(posts);
       res.json({
-        code:200,
-        payload:posts
-      })
+        code: 200,
+        payload: posts,
+      });
     })
-    .catch((error)=>{
+    .catch((error) => {
       console.error(error);
       return res.status(500).json({
-        code:500,
-        message:"서버에러"
-      })
-    })
-})
+        code: 500,
+        message: "서버에러",
+      });
+    });
+});
 
-router.get("/posts/hashtag/:title", verifyToken, async(req,res)=>{
-  try{
-    const hashtag = await Hashtag.findOne({where:{title:req.params.title}})
-    if(!hashtag){
+router.get("/posts/hashtag/:title", verifyToken, async (req, res) => {
+  try {
+    const hashtag = await Hashtag.findOne({
+      where: { title: req.params.title },
+    });
+    if (!hashtag) {
       return res.status(404).json({
-        code:404,
-        message:"검색 결과가 없습니다"
-      })
+        code: 404,
+        message: "검색 결과가 없습니다",
+      });
     }
 
-    const posts = await hashtag.getPosts()
+    const posts = await hashtag.getPosts();
     return res.json({
-      code:200,
-      payload: posts
-    })
-  }catch(error){
-    console.error(error)
+      code: 200,
+      payload: posts,
+    });
+  } catch (error) {
+    console.error(error);
     return res.status(500).json({
-      code:500,
-      message:"서버에러"
-    })
+      code: 500,
+      message: "서버에러",
+    });
   }
-})
+});
 
-
-router.post('/token', async (req, res) => {
+router.post("/token", async (req, res) => {
   const { clientSecret } = req.body;
   try {
     const domain = await Domain.findOne({
       where: { clientSecret },
       include: {
         model: User,
-        attribute: ['nick', 'id'],
+        attribute: ["nick", "id"],
       },
     });
     if (!domain) {
       return res.status(401).json({
         code: 401,
-        message: '등록되지 않은 도메인입니다. 먼저 도메인을 등록하세요',
+        message: "등록되지 않은 도메인입니다. 먼저 도메인을 등록하세요",
       });
     }
-    const token = jwt.sign({
-      id: domain.User.id,
-      nick: domain.User.nick,
-    }, process.env.JWT_SECRET, {
-      expiresIn: '1m', // 1분
-      issuer: 'nodebird',
-    });
+    const token = jwt.sign(
+      {
+        id: domain.User.id,
+        nick: domain.User.nick,
+      },
+      process.env.JWT_SECRET,
+      {
+        expiresIn: "1m", // 1분
+        issuer: "nodebird",
+      }
+    );
     return res.json({
       code: 200,
-      message: '토큰이 발급되었습니다',
+      message: "토큰이 발급되었습니다",
       token,
     });
   } catch (error) {
     console.error(error);
     return res.status(500).json({
       code: 500,
-      message: '서버 에러',
+      message: "서버 에러",
     });
   }
 });
 
-router.get('/test', verifyToken, (req, res) => {
+router.get("/test", verifyToken, (req, res) => {
   res.json(req.decoded);
 });
 
